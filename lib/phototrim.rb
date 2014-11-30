acb4c41abc20b158b01e2bc5cd99ed034e22ae77
Sarup Banskota <sbanskota08@gmail.com>
2014-11-30 16:38:24 +0530

    Modularize into classes, provide args for max_size and root_dir

--- lib/phototrim.rb
+++ lib/phototrim.rb
@@ -4,24 +4,27 @@
 require 'RMagick'
 include Magick
 
-Dir.foreach('images') do |subdir|
-	unless subdir == "." or subdir == ".."
-		print "Processing images within " + subdir.to_s + "\n"
-		Dir.foreach(File.join("images", subdir)) do |victimfile|
-			unless victimfile == "." or victimfile ==  ".." or File.extname(victimfile) == ".svg"
-				print "Processing file " + victimfile + "\n"
-				victim = ImageList.new(File.join("images", subdir, victimfile))
-				if victim.columns > 640
-					print victimfile.to_s + " is beyond 640px wide, scaling down.. \n"
-					victim = victim.scale(640.0/victim.columns)
-					print "Scaled down " + victimfile.to_s + ".\n"
-					victim.write(File.join("images", subdir, victimfile))
-				end 
+class Phototrim
+	def self.trim(max_size, root_dir)
+		Dir.foreach(root_dir) do |subdir|
+			unless subdir == "." or subdir == ".."
+				print "Processing images within " + subdir.to_s + "\n"
+				Dir.foreach(File.join(root_dir, subdir)) do |victimfile|
+					unless victimfile == "." or victimfile ==  ".." or File.extname(victimfile) == ".svg"
+						print "Processing file " + victimfile + "\n"
+						victim = ImageList.new(File.join(root_dir, subdir, victimfile))
+						if victim.columns > max_size
+							print victimfile.to_s + " is beyond " + max_size.to_s + "px wide, scaling down.. \n"
+							victim = victim.scale(max_size.to_f/victim.columns)
+							print "Scaled down " + victimfile.to_s + ".\n"
+							victim.write(File.join(root_dir, subdir, victimfile))
+						end 
+					end
+				end
 			end
 		end
+
+		print "Done! \n"
 	end
 end
 
-print "Done!"
-
-exit
--- phototrim.gemspec
+++ phototrim.gemspec
@@ -1,13 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'phototrim'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2014-12-01'
   s.summary     = "Phototrim"
   s.description = "Scales photos down to neat-ify blogs"
   s.authors     = ["Sarup Banskota"]
   s.email       = 'sarupbanskota@fedoraproject.org'
   s.files       = ["lib/phototrim.rb"]
-  s.homepage    =
-    'http://rubygems.org/gems/phototrim'
+  s.homepage    = 'http://rubygems.org/gems/phototrim'
   s.license       = 'MIT'
 end
