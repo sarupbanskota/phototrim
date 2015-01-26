edc1730012022b633fd85ea6c7a3fb78bf2b5c89
Sarup Banskota <sbanskota08@gmail.com>
2015-01-26 20:55:22 +0530

    Add destination for trimmed files

--- lib/phototrim.rb
+++ lib/phototrim.rb
@@ -6,25 +6,27 @@ require 'RMagick'
 include Magick
 
 class Phototrim
-  def self.trim(max_size, root_path)
-    Dir.foreach root_path do |path_object|
-      unless path_object == "." or path_object == ".." or File.extname(path_object) == ".svg"
-        if File.directory? File.join(root_path, path_object)
-          puts "Processing images within #{path_object}..."
-          trim(max_size, File.join(root_path, path_object))
+  def self.trim(max_size, src_path, dest_path=nil)
+    unless dest_path
+      dest_path = src_path
+    end
+    Dir.foreach src_path do |src_object|
+      unless src_object == "." or src_object == ".." or File.extname(src_object) == ".svg"
+        if File.directory? File.join(src_path, src_object)
+          puts "Processing images within #{src_object}..."
+          trim(max_size, File.join(src_path, src_object), File.join(dest_path, src_object))
         else
-          puts "Processing file #{path_object}"
-          victim = ImageList.new(File.join(root_path, path_object))
+          puts "Processing file #{src_object}"
+          victim = ImageList.new(File.join(src_path, src_object))
           if victim.columns > max_size
-            puts "#{path_object} is beyond #{max_size}px wide, scaling down.."
+            puts "#{src_object} is beyond #{max_size}px wide, scaling down.."
             victim = victim.scale(max_size.to_f/victim.columns)
-            puts "Scaled down #{path_object}"
-            victim.write(File.join(root_path, path_object))
+            puts "Scaled down #{src_object}"
+            victim.write(File.join(dest_path, src_object))
           end
         end
       end
     end
-    puts "Done!"
   end
 end
 
