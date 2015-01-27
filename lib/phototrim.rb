bf0953f577eb19d8d06068b69fc69d08e08e7f5b
Sarup Banskota <sbanskota08@gmail.com>
2015-01-27 15:14:50 +0530

    Cleaner code

--- lib/phototrim.rb
+++ lib/phototrim.rb
@@ -5,37 +5,48 @@ require 'phototrim/version'
 require 'RMagick'
 include Magick
 
-class Phototrim
-  def self.trim max_size, src_photo_path, dest_photo_path
-    puts "Processing file #{src_photo_path}"
-    victim = ImageList.new src_photo_path
-    if victim.columns > max_size
-      puts "#{src_photo_path} is beyond #{max_size}px wide, scaling down.."
-      victim = victim.scale(max_size.to_f/victim.columns)
-      puts "Scaled down #{src_photo_path}"
-      victim.write dest_photo_path
-    end
+class Phototrimmer
+
+  def initialize max_size, src_path, dest_path=nil
+    @size = max_size
+    @src = src_path
+    @dest = dest_path.nil? ? @src : dest_path
   end
 
-  def self.wrangle max_size, src_path, dest_path
-    Dir.foreach src_path do |src_object|
-      # reduce that check into a method
-      unless src_object == "." or src_object == ".." or File.extname(src_object) == ".svg"
-        if File.directory? File.join(src_path, src_object)
-          puts "Processing images within #{src_object}..."
-          system "mkdir -p #{File.join(dest_path, src_object)}"
-          wrangle max_size, File.join(src_path, src_object), File.join(dest_path, src_object)
-        else
-          trim max_size, File.join(src_path, src_object), File.join(dest_path, src_object)
-        end
+  def main
+    system("mkdir -p #{@dest}")
+    File.directory? @src ? wrangle : trim
+  end
+
+  def wrangle
+    Dir.foreach @src do |object|
+      next if ignored_type object
+      subtrimmer = Phototrimmer.new @size, 
+                                    File.join(@src, object), 
+                                    File.join(@dest, object)
+      if File.directory? File.join @src, object
+        puts "Processing images within #{object}..."
+        system "mkdir -p #{ File.join @dest, object }"
+        subtrimmer.wrangle
+      else
+        subtrimmer.trim
       end
     end
   end
 
-  def self.main max_size, src_path, dest_path=nil
-    dest_path ? system("mkdir -p #{dest_path}") : (dest_path = src_path)
-    File.directory?(src_path) ? wrangle(max_size, src_path, dest_path) : trim(max_size, src_path, dest_path)
+  def trim
+    puts "Processing file #{@src}"
+    victim = ImageList.new @src
+    if victim.columns > @size
+      puts "#{@src} is beyond #{@size}px wide, scaling down.."
+      victim = victim.scale @size.to_f/victim.columns
+      puts "Scaled down #{@src}"
+      victim.write @dest
+    end
+  end
+
+  def ignored_type object
+    %w[. ..].include? object or %w[.svg].include? File.extname object
   end
 
 end
-
