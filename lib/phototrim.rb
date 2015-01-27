7002e4091f38fa75cf45dff8f0244d6bf4f45b8d
Sarup Banskota <sbanskota08@gmail.com>
2015-01-27 09:14:07 +0530

    Break code into chunks

--- lib/phototrim.rb
+++ lib/phototrim.rb
@@ -6,28 +6,36 @@ require 'RMagick'
 include Magick
 
 class Phototrim
-  def self.trim(max_size, src_path, dest_path=nil)
-    unless dest_path
-      dest_path = src_path
+  def self.trim max_size, src_photo_path, dest_photo_path
+    puts "Processing file #{src_photo_path}"
+    victim = ImageList.new src_photo_path
+    if victim.columns > max_size
+      puts "#{src_photo_path} is beyond #{max_size}px wide, scaling down.."
+      victim = victim.scale(max_size.to_f/victim.columns)
+      puts "Scaled down #{src_photo_path}"
+      victim.write dest_photo_path
     end
+  end
+
+  def self.wrangle max_size, src_path, dest_path
     Dir.foreach src_path do |src_object|
+      # reduce that check into a method
       unless src_object == "." or src_object == ".." or File.extname(src_object) == ".svg"
         if File.directory? File.join(src_path, src_object)
           puts "Processing images within #{src_object}..."
           system "mkdir -p #{File.join(dest_path, src_object)}"
-          trim(max_size, File.join(src_path, src_object), File.join(dest_path, src_object))
+          wrangle max_size, File.join(src_path, src_object), File.join(dest_path, src_object)
         else
-          puts "Processing file #{src_object}"
-          victim = ImageList.new(File.join(src_path, src_object))
-          if victim.columns > max_size
-            puts "#{src_object} is beyond #{max_size}px wide, scaling down.."
-            victim = victim.scale(max_size.to_f/victim.columns)
-            puts "Scaled down #{src_object}"
-            victim.write(File.join(dest_path, src_object))
-          end
+          trim max_size, File.join(src_path, src_object), File.join(dest_path, src_object)
         end
       end
     end
   end
+
+  def self.main max_size, src_path, dest_path=nil
+    dest_path ? system("mkdir -p #{dest_path}") : (dest_path = src_path)
+    File.directory?(src_path) ? wrangle(max_size, src_path, dest_path) : trim(max_size, src_path, dest_path)
+  end
+
 end
 
